c9ce3b4b409501d6a038123299828a836359b33e
Ethan Rudy <16.erudy@gmail.com>
2022-06-14 16:19:05 -0700

    Create constants.h

---
+++ Headers/constants.h
@@ -0,0 +1,7 @@
+// File for simulation constants
+// @file constants.h
+// @author Ethan Rudy
+
+const int SIZE = 100;	// Grid size
+const int SCALE = 12;	// Grid square scale
+const int dt = 3;	// Simulation speed
